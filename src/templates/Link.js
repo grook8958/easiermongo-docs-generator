417803921e25c0dae0cfb202a0983b76d6aad082
grook8958 <clement.puks@gmail.com>
2022-11-08 10:30:06 +0100

    (refactor): Link Generator reworked to handle any links

--- src/templates/Link.js
+++ src/templates/Link.js
@@ -2,29 +2,135 @@ const { EXTERNAL_URL_MAPPER, LESS_THAN, GREATER_THAN } = require("../utils/Const
 const DocumentURLMapper = require("../utils/DocumentURLMapper");
 const { replaceAll } = require("../utils/Util");
 
-exports.template = `<a href="[link-href]">[link-name]</a>`;
-exports.template__external = `<a target="_blank" rel="noopener" href="[link-href]">[link-name] <svg preserveAspectRatio="xMidYMid meet" class="mb-1 weight-500" viewBox="0 0 24 24" width="1.3em" height="1.3em"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-4M14 4h6m0 0v6m0-6L10 14"></path></svg></a>`;
+exports = module.exports = class Link {
+    constructor(baseTypes) {
+        /**
+         * @type {string}
+         */
+        this.baseTypes = baseTypes;
 
-exports.generateURL = (name) => {
-    if (DocumentURLMapper.getDoc(name)) {
-        return DocumentURLMapper.getDoc(name).href;
-    } else if (Object.keys(EXTERNAL_URL_MAPPER).includes(name.toLowerCase())) {
-        return EXTERNAL_URL_MAPPER[name];
-    } else throw new TypeError('[INVALID_TYPE] URL Type is invalid.')
-}
+        /**
+         * @type {{types: Array<string|Array<string>>, subSeparator: string}}
+         */
+        this.__resolvedTypes = this.resolveBaseTypes();
 
-exports.parseVars = (linkHref, linkName) => {
-    let parsed = this.template;
-    parsed = replaceAll(parsed, '[link-href]', linkHref);
-    parsed = replaceAll(parsed, '[link-name]', linkName);
-    return parsed;
-}
+        /**
+         * @type {{types: Array<string|Array<string>>, subSeparator: string}}
+         */
+        this.__resolvedURLTypes = this.parseURL();
 
-exports.parseVars__external = (linkHref, linkName) => {
-    let parsed = this.template__external;
-    parsed = replaceAll(parsed, '[link-href]', linkHref);
-    parsed = replaceAll(parsed, '[link-name]', linkName); 
-    return parsed;
+        this.link = this.toHTMLLink();
+    }
+
+    /**
+     * 
+     * @param {string} baseTypes 
+     * @returns {{types: Array<string|Array<string>>, subSeparator: string}}
+     */
+    resolveBaseTypes(baseTypes = this.baseTypes) {
+        //Collection<number,string>
+        //Array<number|boolean>
+        //Promise<Array<string>>
+        //number|string
+        let subSeparator = null;
+        //[Array, number|boolean]
+        //[Promise, Array, string|number]
+        const split = baseTypes.split('<').map(item => item.replace(/>/gm, ''));
+        for (const item of split) {
+            //[Promise, Array, [string, number]]
+            if (item.includes('|')) {
+                split.splice(split.indexOf(item), 1, item.split('|'));
+                subSeparator = '|';
+            } else if (item.includes(',')) {
+                split.splice(split.indexOf(item), 1, item.split(','));
+                subSeparator = ',';
+            }
+        }
+        return {
+            types: split,
+            subSeparator
+        }   
+    }
+
+    parseURL(resolvedTypes = this.__resolvedTypes) {
+        const types = resolvedTypes.types.map(item => {
+            if (Array.isArray(item)) {
+                return item.map(i => { 
+                    const url = Link.generateURL(i)
+                    if (Object.keys(EXTERNAL_URL_MAPPER).includes(i.toLowerCase())) {
+                        return Link.parseVars__external(url, i);
+                    } else {
+                        return Link.parseVars(url, i);
+                    }
+                });
+            } else {
+                const url = Link.generateURL(item)
+                if (Object.keys(EXTERNAL_URL_MAPPER).includes(item.toLowerCase())) {
+                    return Link.parseVars__external(url, item);
+                } else {
+                    return Link.parseVars(url, item);
+                }
+            }
+        });
+        return {
+            types,
+            subSeparator: resolvedTypes.subSeparator,
+        }
+    }
+
+    toHTMLLink(resolvedURLTypes = this.__resolvedURLTypes) {
+        let str = '';
+        for (const item of resolvedURLTypes.types) {
+            if (Array.isArray(item)) {
+                str += `${LESS_THAN}${item.join(resolvedURLTypes.subSeparator)}`;
+            } else {
+                if (resolvedURLTypes.types.indexOf(item) === 0) {
+                    str += item;
+                } else {
+                    str += `${LESS_THAN}${item}`;
+                }
+            }
+        }
+        for (let i = 0; i < resolvedURLTypes.types.length - 1; i++) {
+            str += GREATER_THAN;
+        }
+        
+        return str;
+    }
+
+    static parseVars (linkHref, linkName) {
+        let parsed = Link.template;
+        parsed = replaceAll(parsed, '[link-href]', linkHref);
+        parsed = replaceAll(parsed, '[link-name]', linkName);
+        return parsed;
+    }
+    
+    static parseVars__external (linkHref, linkName) {
+        let parsed = Link.template__external;
+        parsed = replaceAll(parsed, '[link-href]', linkHref);
+        parsed = replaceAll(parsed, '[link-name]', linkName); 
+        return parsed;
+    }
+
+    static generateURL = (name) => {
+        if (DocumentURLMapper.getDoc(name)) {
+            return DocumentURLMapper.getDoc(name).href;
+        } else if (Object.keys(EXTERNAL_URL_MAPPER).includes(name.toLowerCase())) {
+            return EXTERNAL_URL_MAPPER[name.toLowerCase()];
+        } else {
+            console.log(Object.keys(EXTERNAL_URL_MAPPER))
+            console.log(name.toLowerCase())
+            throw new TypeError('[INVALID_TYPE] URL Type is invalid.: ' + name)
+        }
+    }
+
+    static get template() {
+        return `<a href="[link-href]">[link-name]</a>`;
+    }
+
+    static get template__external() {
+        return `<a target="_blank" rel="noopener" href="[link-href]">[link-name] <svg preserveAspectRatio="xMidYMid meet" class="mb-1 weight-500" viewBox="0 0 24 24" width="1.3em" height="1.3em"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-4M14 4h6m0 0v6m0-6L10 14"></path></svg></a>`;        
+    }
 }
 
 /**
@@ -33,13 +139,23 @@ exports.parseVars__external = (linkHref, linkName) => {
  * @returns 
  */
 exports.new = (name) => {
+    
+    ['Array', '<', 'number', '|', 'boolean', '>']
     if (name.includes('|')) {
         const names = name.split('|');
         const html = [];
         for (const itemName of names) {
-            html.push(this.new(itemName))
+            html.push(this.new(itemName.replace(/>/gm, '')))
         }
         return html.join('|');
+    } else if (name.includes(',')) {
+        const names = name.split(',');
+        const html = [];
+        for (const itemName of names) {
+            console.log(itemName)
+            html.push(this.new(itemName.replace(/>/gm, '')))
+        }
+        return html.join(',');
     } else if (name.includes('<')) {
         const names = name.replace(/>/gm, '')
         const arrNames = names.split('<');
