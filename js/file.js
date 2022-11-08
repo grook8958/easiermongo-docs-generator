397e30e0854baf251abdba132708b2ef01f5f448
grook8958 <clement.puks@gmail.com>
2022-11-08 11:00:18 +0100

    test: re-generated tests

--- js/file.js
+++ js/file.js
@@ -1,3 +1,13 @@
+'use-strict';
+
+const { Collection } = require('@discordjs/collection');
+const { TypeError } = require('../errors');
+const Utils = require('../util/Utils');
+const MongoDocument = require('./MongoDocument');
+const DocumentBuilder = require('../builders/DocumentBuilder');
+const ms = require('ms');
+const DocumentExpiryManager = require('./DocumentExpiryManager');
+
 /**
  * The representation of a model
  */
@@ -44,7 +54,6 @@ class MongoModel {
 	/**
 	 * Resolves the TTL to a number
 	 * @private
-     * @returns {number}
 	 */
 	resolveTTL() {
 		const paths = this._model.schema.paths;
@@ -62,7 +71,6 @@ class MongoModel {
 	 * Runs the callback each time a document expires.
 	 * * Documents created BEFORE the code was ran are not taken into notice
 	 * @param {(id: string, document: MongoDocument) => {}} callback The function to run when a document expires
-     * @returns {void}
 	 */
 	onExpire(callback) {
 		if (typeof callback === 'function') {
@@ -96,7 +104,7 @@ class MongoModel {
 
 	/**
 	 * Get all the documents in this collection.
-	 * @returns {Promise<MongoDocument>} The document(s) found in the collection
+	 * @returns {Promise<MongoDocument|null>} The document(s) found in the collection
 	 */
 	async getAll() {
 		const docs = await this._model.find();
@@ -108,7 +116,7 @@ class MongoModel {
 	/**
 	 * Get a document by it's id
 	 * @param {string} id The document `_id`
-	 * @returns {Promise<MongoDocument>} The document matching the id
+	 * @returns {Promise<MongoDocument|null>} The document matching the id
 	 * @example
 	 * const document = await model.get('someId')
 	 * 	.catch(err => console.error(err))
@@ -127,7 +135,7 @@ class MongoModel {
 	 * Searches for one document matching the query
 	 * * If you want to find a document with the `_id`, use `get` method instead.
 	 * @param {MongoQuery} query The query to search for
-	 * @returns {Promise<MongoDocument>} The document matching the query
+	 * @returns {Promise<MongoDocument|null>} The document matching the query
 	 * @example
 	 * const document = await model.find({ name: 'John', lastName: 'Doe' })
 	 * 	.catch(err => console.error(err))
@@ -146,7 +154,7 @@ class MongoModel {
 	 * Searches for all document matching the query
 	 * * If you want to find a document with the `_id`, use `get` method instead.
 	 * @param {MongoQuery} query The query to search for
-	 * @returns {Promise<Array<MongoDocument>>} The document(s) matching the query
+	 * @returns {Promise<Array<MongoDocument|null>>} The document(s) matching the query
 	 */
 	async findMany(query) {
 		if (typeof query !== 'object') throw new TypeError('INVALID_TYPE', 'query', 'object');
@@ -166,7 +174,7 @@ class MongoModel {
 	 * @param {string} id The document's `_id`
 	 * @param {MongoChange} change The changes it should apply to the document
 	 * @param {ModelEditOptions} options The options of this edit
-	 * @returns {Promise<MongoDocument>} The old document or if `options.new` is set to `true` then it will return the newly edited document.
+	 * @returns {Promise<MongoDocument|null>} The old document or if `options.new` is set to `true` then it will return the newly edited document.
 	 * @example
 	 * //Change the age to 48.
 	 * //Set option `new` to true to return the edited document
@@ -178,7 +186,7 @@ class MongoModel {
 		if (typeof id !== 'string') throw new TypeError('INVALID_TYPE', 'id', 'string');
 		if (typeof change !== 'object') throw new TypeError('INVALID_TYPE', 'change', 'object');
 		if (typeof options !== 'object') throw new TypeError('INVALID_TYPE', 'options', 'object');
-		const rawDoc = await this._model.findByIdAndUpdate(id, change, options)
+		const rawDoc = await this._model.findByIdAndUpdate(id, change, options);
 		if (!rawDoc) return null;
 		const doc = new MongoDocument(rawDoc, this);
 		if (doc && options.new === true) this.cache.set(doc._id, doc);
@@ -190,14 +198,14 @@ class MongoModel {
 	 * @param {MongoQuery} query The query to search for
 	 * @param {MongoChange} change The changes it should apply to the document
 	 * @param {ModelEditOptions} options The options of this edit
-	 * @returns {Promise<MongoDocument>} The old document or if `options.new` is set to `true` then it will return the newly edited document.
+	 * @returns {Promise<MongoDocument|null>} The old document or if `options.new` is set to `true` then it will return the newly edited document.
 	 */
 	async findAndEdit(query, change, options = {}) {
 		if (typeof query !== 'object') throw new TypeError('INVALID_TYPE', 'query', 'object');
 		if (typeof change !== 'object') throw new TypeError('INVALID_TYPE', 'change', 'object');
 		if (typeof options !== 'object') throw new TypeError('INVALID_TYPE', 'options', 'object');
 		const rawDoc = await this._model.findOneAndUpdate(query, change, options);
-		if (!rawDoc) return null; 
+		if (!rawDoc) return null;
 		const doc = new MongoDocument(rawDoc, this);
 		if (doc && options.new === true) this.cache.set(doc._id, doc);
 		return doc;
@@ -208,7 +216,7 @@ class MongoModel {
 	 * @param {MongoQuery} query The query to search for
 	 * @param {MongoChange} change The changes that should apply to all the documents matching the query
 	 * @param {ModelEditOptions} options The options of this edit
-	 * @returns {Promise<Array<MongoDocument>>} The old document(s) or if `options.new` is set to `true` then it will return the newly edited document(s).
+	 * @returns {Promise<Array<MongoDocument|null>>} The old document(s) or if `options.new` is set to `true` then it will return the newly edited document(s).
 	 */
 	async editMany(query, change, options = {}) {
 		if (typeof query !== 'object') throw new TypeError('INVALID_TYPE', 'query', 'object');
@@ -217,7 +225,7 @@ class MongoModel {
 		const docs = await this._model.updateMany(query, change, options);
 		if (docs.length > 0 && options.new === true) docs.forEach((doc) => this.cache.set(doc._id, doc));
 		if (docs.length === 0 || !docs) return null;
-		return docs.map((d) => new MongoDocument(d, this));;
+		return docs.map((d) => new MongoDocument(d, this));
 	}
 
 	/**
@@ -268,7 +276,7 @@ class MongoModel {
 	 * @param {string} id The id of the document to update
 	 * @param {MongoChange} change What to change in the document
 	 * @param {ModelEditOptions} options The options of this edit
-	 * @returns {Promise<MongoDocument>} The old document or if `options.new` is set to `true` then it will return the newly edited document.
+	 * @returns {Promise<MongoDocument|null>} The old document or if `options.new` is set to `true` then it will return the newly edited document.
 	 */
 	async update(id, change, options = {}) {
 		if (typeof id !== 'string') throw new TypeError('INVALID_TYPE', 'id', 'string');
@@ -285,7 +293,7 @@ class MongoModel {
 	 * @param {MongoQuery} query The to search for
 	 * @param {MongoChange} change What to change in the document
 	 * @param {ModelEditOptions} options The options of this edit
-	 * @returns {Promise<MongoDocument>} The old document or if `options.new` is set to `true` then it will return the newly edited document.
+	 * @returns {Promise<MongoDocument|null>} The old document or if `options.new` is set to `true` then it will return the newly edited document.
 	 */
 	async findAndUpdate(query, change, options = {}) {
 		if (typeof query !== 'object') throw new TypeError('INVALID_TYPE', 'query', 'object');
@@ -302,7 +310,7 @@ class MongoModel {
 	 * @param {MongoQuery} query The to search for
 	 * @param {MongoChange} change What to change in the document
 	 * @param {ModelEditOptions} options The options of this edit
-	 * @returns {Promise<Array<MongoDocument>>} The old documents or if `options.new` is set to `true` then it will return the newly edited documents.
+	 * @returns {Promise<Array<MongoDocument|null>>} The old documents or if `options.new` is set to `true` then it will return the newly edited documents.
 	 */
 	async updateMany(query, change, options = {}) {
 		if (typeof query !== 'object') throw new TypeError('INVALID_TYPE', 'query', 'object');
